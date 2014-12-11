b4869878ef68178949925d02e6c8fa75835bde28
Matthew Phillips <hi@mattp.me>
2014-12-11 23:08:30 +0000

    [MP] Added the son file instead of js for the batman data

--- controller/promiseController.js
+++ controller/promiseController.js
@@ -1,7 +1,7 @@
 angular.module('appController', [])
 	.controller('promiseController', function ($scope, $http) {
 		console.log('controller on');
-		$http.get('model/batman.js').success(function(data) {
+		$http.get('model/batman.json').success(function(data) {
 	    	$scope.characters = data;
 	    })
 	    .error(function(err) {
